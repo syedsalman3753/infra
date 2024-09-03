72f6b626d67f85215f2132178798c8fdab2bf286
syedsalman3753 <syedsalman3753@users.noreply.github.com>
2024-09-03 13:17:44 +0000

    Terraform files GitHub Actions
    Signed-off-by: syedsalman3753 <syedsalman3753@users.noreply.github.com>

--- terraform/aws/modules/nfs-setup/main.tf
+++ terraform/aws/modules/nfs-setup/main.tf
@@ -8,9 +8,9 @@ variable "NFS_SERVER_LOCATION" {
 }
 variable "NFS_SERVER" {
   description = "The NFS server address, which can be a DNS name, an IPv4 address, or an IPv6 address."
-  type = string
+  type        = string
   validation {
-    condition = can(regex("^[a-zA-Z0-9.-]+$", var.NFS_SERVER)) || can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.NFS_SERVER)) || can(regex("^(::|[a-fA-F0-9]{1,4}::?|::[a-fA-F0-9]{1,4})([a-fA-F0-9]{1,4}:){0,6}[a-fA-F0-9]{1,4}$", var.NFS_SERVER))
+    condition     = can(regex("^[a-zA-Z0-9.-]+$", var.NFS_SERVER)) || can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.NFS_SERVER)) || can(regex("^(::|[a-fA-F0-9]{1,4}::?|::[a-fA-F0-9]{1,4})([a-fA-F0-9]{1,4}:){0,6}[a-fA-F0-9]{1,4}$", var.NFS_SERVER))
     error_message = "The NFS_SERVER must be a valid DNS name, an IPv4 address, or an IPv6 address."
   }
 }
@@ -31,20 +31,20 @@ variable "K8S_INFRA_BRANCH" {
 variable "CLUSTER_NAME" { type = string }
 
 locals {
-  NFS_CONFIG={
-    K8S_INFRA_REPO_URL=var.K8S_INFRA_REPO_URL
-    K8S_INFRA_BRANCH=var.K8S_INFRA_BRANCH
-    NFS_SERVER=var.NFS_SERVER
-    NFS_SERVER_LOCATION=var.NFS_SERVER_LOCATION
-    K8S_INFRA_NFS_LOCATION="./k8s-infra/storage-class/nfs"
-    K8S_INFRA_NFS_SERVER_SCRIPT_NAME="install-nfs-server.sh"
-    K8S_INFRA_NFS_CSI_SCRIPT_NAME="install-nfs-csi.sh"
+  NFS_CONFIG = {
+    K8S_INFRA_REPO_URL               = var.K8S_INFRA_REPO_URL
+    K8S_INFRA_BRANCH                 = var.K8S_INFRA_BRANCH
+    NFS_SERVER                       = var.NFS_SERVER
+    NFS_SERVER_LOCATION              = var.NFS_SERVER_LOCATION
+    K8S_INFRA_NFS_LOCATION           = "./k8s-infra/storage-class/nfs"
+    K8S_INFRA_NFS_SERVER_SCRIPT_NAME = "install-nfs-server.sh"
+    K8S_INFRA_NFS_CSI_SCRIPT_NAME    = "install-nfs-csi.sh"
 
-    NFS_SERVER_LOG_FILE_PATH="/tmp/nfs-server-log"
-    NFS_CSI_LOG_FILE_PATH="/tmp/nfs-csi-log"
+    NFS_SERVER_LOG_FILE_PATH = "/tmp/nfs-server-log"
+    NFS_CSI_LOG_FILE_PATH    = "/tmp/nfs-csi-log"
 
-    HELM_VERSION="helm-v3.15.4-linux-amd64.tar.gz"
-    CLUSTER_NAME=var.CLUSTER_NAME
+    HELM_VERSION = "helm-v3.15.4-linux-amd64.tar.gz"
+    CLUSTER_NAME = var.CLUSTER_NAME
 
   }
   NFS_ENV_VARS = [
@@ -62,7 +62,7 @@ resource "null_resource" "nfs-server-setup" {
   }
   provisioner "remote-exec" {
     inline = concat(
-      [ "echo \"export NFS_SERVER_LOCATION=${var.NFS_SERVER_LOCATION}\" | sudo tee -a /etc/environment",
+      ["echo \"export NFS_SERVER_LOCATION=${var.NFS_SERVER_LOCATION}\" | sudo tee -a /etc/environment",
         "sudo source /etc/environment",
         "sudo bash ${local.NFS_CONFIG.K8S_INFRA_NFS_LOCATION}/${local.NFS_CONFIG.K8S_INFRA_NFS_SERVER_SCRIPT_NAME} | tee -a ${local.NFS_CONFIG.NFS_SERVER_LOG_FILE_PATH}-$( date +\"%d-%h-%Y-%H-%M\" ).log"
       ]
